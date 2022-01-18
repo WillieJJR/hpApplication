0153ccfa31c6a0bfe276abc4f39f6b513d7f5089
unknown <williejc@ymail.com>
2022-01-17 21:46:33 -0500

    staging nltk downloader

--- hpAnalysisApp.py
+++ hpAnalysisApp.py
@@ -103,7 +103,7 @@ hp_df_final = hp_df.merge(spells_df, on='Incantation', how='left')
 #need to download nltk for tokenizer
 nltk.download('omw-1.4')
 nltk.download('punkt')
-nltk.download()
+nltk.download('stopwords')
 
 '''This is where application development begins using Dash framework'''
 
